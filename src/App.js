6c863fd7a03dd30f4d6f70d5068ed920f5200b5a
disconaked <nolan.klinke@gmail.com>
2018-10-04 20:37:42 -0400

    added basic page structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Card, Row, Col } from 'react-materialize';
 
 class App extends Component {
   render() {
